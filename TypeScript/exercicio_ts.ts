87f3777906c24a1840e0b6c5ef535bd74457dc44
Bruno Guimarães <brunogdepaula@hotmail.com>
2023-11-28 15:43:55 -0300

    entrega do exercicio

---
+++ TypeScript/exercicio_ts.ts
@@ -0,0 +1,15 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultado = multiplicar(2, 2);
+const mensagem = saudacao("Bruno");
+
+console.log(resultado);
+console.log(mensagem); 
